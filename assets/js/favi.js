57e8fb8392836b5002d1db70e26cff94e1eab215
shoplibbi <79004627+shoplibbi@users.noreply.github.com>
2021-05-02 03:28:49 -0400

    Create favi.js

---
+++ assets/js/favi.js
@@ -0,0 +1,7 @@
+var link = document.querySelector("link[rel~='image/png']");
+if (!link) {
+    link = document.createElement('link');
+    link.rel = 'image/png';
+    document.getElementsByTagName('head')[0].appendChild(link);
+}
+link.href = 'https://shoplibbi.github.io/assets/img/32px.png';
